cdbe05a3e06984faa682a0166fee7bd3b53953a0
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-11-16 18:58:35 +0100

    using transform matrices seems to work?

--- App.js
+++ App.js
@@ -3,27 +3,50 @@ import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 import EditTextView from './EditTextView';
 
 // Already ~8 children are enough to cause the issue
-const hundredItems = Array.from({length: 100}, (_, i) => i);
+const hundredItems = Array.from({length: 8}, (_, i) => i);
 
 const renderItem = ({item}) => <Text style={styles.text}>Item #{item}</Text>;
 
 // The inverted prop adds a style to the FlatList as array,
 // and somehow thats causing the issue i believe. When not setting
 // the inverted prop, the issue is not present.
-export default () => {
+const App = () => {
   return (
-    <SafeAreaView style={styles.flex1}>
-      <View style={styles.list}>
-        {hundredItems.map(item => (
-          <View key={item} style={styles.item}>
-            {renderItem({item})}
-          </View>
-        ))}
+    <View style={styles.flex1}>
+      <View
+        style={{
+          flex: 1,
+          transform: [{ matrix: [ // seems to work
+              -1, 0, 0, 0,
+              0, 1, 0, 0,
+              0, 0, 1, 0,
+              0, 0, 0, -1
+            ]
+          }],
+        }}>
+        <View
+          style={{
+            transform: [{ matrix: [ // seems to work
+                -1, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, -1
+              ]
+            }],
+          }}>
+          {hundredItems.reverse().map(item => (
+            <View key={item} style={styles.item}>
+              {renderItem({item})}
+            </View>
+          ))}
+        </View>
       </View>
       <EditTextView style={styles.input} />
-    </SafeAreaView>
+    </View>
   );
 };
+
+export default App;
 
 const styles = StyleSheet.create({
   flex1: {
@@ -39,12 +62,5 @@ const styles = StyleSheet.create({
     height: 50,
     width: '100%',
     borderWidth: StyleSheet.hairlineWidth,
-  },
-  list: {
-    flex: 1,
-    transform: [{rotate: '180deg'}],
-  },
-  item: {
-    transform: [{rotate: '180deg'}],
   },
 });
